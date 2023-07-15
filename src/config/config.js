f34ffc3286193cc547f4a33678413361b4429938
Deepak B <deepakbhangale1996@gmail.com>
2023-07-15 22:58:47 +0400

    adsad

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,12 @@ let backendHost = {
   getAPIUrl: () => {
     if (hostname === "localhost") {
       return `https://backend.siz.ae/api`;
+     //return `http://13.49.49.212:8085/api`;
     } else if (hostname === "siz-front-end.web.app") {
       return `https://backend.siz.ae/api`;
+     //return `http://13.49.49.212:8085/api`;
     } else return "https://backend.siz.ae/api";
+    //else return `http://13.49.49.212:8085/api`;
   },
 };
 
